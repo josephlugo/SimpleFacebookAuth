7975817b917fa74f9bc7b2d7b555ba26c9513701
jlugooi - Jose Lugo <jalugogarcia@gmail.com>
2017-03-13 22:54:22 -0400

    Added license in source code

--- SimpleFacebookAuth/Default.aspx.vb
+++ SimpleFacebookAuth/Default.aspx.vb
@@ -13,6 +13,9 @@ Imports Facebook
 '        First one for the redirect login page, (Ex. http://www.mywebsite.com/Authenticated_User)
 '        the second one for the redirect page once user logged off (Ex. http://www.mywebsite.com/)
 
+'<a rel = "license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a><br /><span xmlns:dct = "http://purl.org/dc/terms/" Property="dct:title">Simple Facebook Authentication</span> by <a xmlns:cc = "http://creativecommons.org/ns#" href="https://www.linkedin.com/in/jalugo/" Property="cc:attributionName" rel="cc:attributionURL">Jose Alejandro Lugo Garcia</a> Is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />Based On a work at <a xmlns:dct = "http://purl.org/dc/terms/" href="https://github.com/jlugooi/SimpleFacebookAuth" rel="dct:source">https://github.com/jlugooi/SimpleFacebookAuth</a>.
+
+
 Public Class _Default
     Inherits Page
 
